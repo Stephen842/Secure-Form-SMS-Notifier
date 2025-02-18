06084e8973a3002aeb132f25657bcab50d5f6f15
Stephen842 <ugotachisomstephen@gmail.com>
2025-02-18 02:45:15 +0100

    added a template file and I am about to start working on my frontend interface

--- encrypted_notification/Core/urls.py
+++ encrypted_notification/Core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('Dashboard/<str:token>/', views.dashboard, name='dashboard'),
     path('messages/<str:token>/', views.message_view, name='messages'),
     path('invalid_token_url/', views.expired_view, name='invalid_token_url'),
-
+    path('message_sent_successfully/', views.success, name='success_url')
 ]
